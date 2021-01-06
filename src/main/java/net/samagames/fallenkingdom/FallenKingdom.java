fea9b89e2ca84b54e6f700963776aeef27db504f
nguyenvantruong172yt1p <wangwang698@yeah.net>
2021-01-06 20:04:54 +0800

    start to work

---
+++ src/main/java/net/samagames/fallenkingdom/FallenKingdom.java
@@ -0,0 +1,31 @@
+package net.samagames.fallenkingdom;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * ╱╲＿＿＿＿＿＿╱╲
+ * ▏╭━━╮╭━━╮▕
+ * ▏┃＿＿┃┃＿＿┃▕
+ * ▏┃＿▉┃┃▉＿┃▕
+ * ▏╰━━╯╰━━╯▕
+ * ╲╰╰╯╲╱╰╯╯╱  Created by Silvanosky on 04/07/2016
+ * ╱╰╯╰╯╰╯╰╯╲
+ * ▏▕╰╯╰╯╰╯▏▕
+ * ▏▕╯╰╯╰╯╰▏▕
+ * ╲╱╲╯╰╯╰╱╲╱
+ * ＿＿╱▕▔▔▏╲＿＿
+ * ＿＿▔▔＿＿▔▔＿＿
+ */
+public class FallenKingdom extends JavaPlugin{
+
+    public void onEnable()
+    {
+        //TODO
+    }
+
+    public void onDisable()
+    {
+        //TODO
+    }
+}
+
